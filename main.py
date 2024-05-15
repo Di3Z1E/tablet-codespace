dde61bc45f24321c41c2eb70b2ea98c525238f19
Di3Z1E <100869868+Di3Z1E@users.noreply.github.com>
2024-05-15 07:52:47 +0000

    Added DOckerfiles

---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Flask
+import requests
+
+
+def main() -> None:
+    pass
+
+
+class App:
+    def __init__(self):
+        self.app = ...
+
+    @classmethod
+    def initiate_rest_server(self):
+        pass
+
+
+if __name__ == "__main__":
+    main()
